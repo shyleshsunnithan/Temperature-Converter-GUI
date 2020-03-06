68328fec9f94260940ba66e787cd4a1783b6e04e
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-03-06 14:24:06 +0530

    Updated font

--- Temperature Converter/temperature.py
+++ Temperature Converter/temperature.py
@@ -14,16 +14,16 @@ def farhenheit_to_celsius():
     answer = (value - 32)*5/9
     showinfo("Answer",f"{value} F = {answer} C") 
 
-label = Label(win,text="Enter Temperature Here:",font=('fira sans',15,'bold'))
+label = Label(win,text="Enter Temperature Here:",font=('arial',15,'bold'))
 label.grid(row=0,column=0)
 
-entry = Entry(win,font=('fira sans',20,'bold'))
+entry = Entry(win,font=('arial',20,'bold'))
 entry.grid(row=1,column=0)
 
-button1 = Button(win,text="Celsius to Fahrenheit",font=('fira sans',15,'bold'),command=celsius_to_farhenheit)
+button1 = Button(win,text="Celsius to Fahrenheit",font=('arial',15,'bold'),command=celsius_to_farhenheit)
 button1.grid(row=3,column=0)
 
-button2 = Button(win,text="Fahrenheit to Celsius",font=('fira sans',15,'bold'),command=farhenheit_to_celsius)
+button2 = Button(win,text="Fahrenheit to Celsius",font=('arial',15,'bold'),command=farhenheit_to_celsius)
 button2.grid(row=4,column=0)
 
 
